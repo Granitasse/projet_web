45779c5182a8c088e2ede1d7d1f373c098b79c5b
Bruce <bruce.tripica@gmail.com>
2025-04-21 16:52:44 +0200

    Replace In memory manager by database

--- src/main/java/com/example/myservice/service/UserService.java
+++ src/main/java/com/example/myservice/service/UserService.java
@@ -1,16 +1,31 @@
 package com.example.myservice.service;
 
+import com.example.myservice.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-//@Service
-//public class UserService implements UserDetailsService {
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        return null;
-//    }
-//}
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
 
-public class UserService {}
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("coucou");
+        var user = userRepository.getUserByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        String[] authorities = user.isAdmin() ? new String[]{"ADMIN"} : new String[0];
+
+        return User.builder()
+                   .username(user.getUsername())
+                   .password(user.getPassword())
+                   .authorities(authorities)
+                   .build();
+    }
+}
